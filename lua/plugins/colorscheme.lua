8d6194079d7b1e8ae080948d60ab41dfae9df2e3
harithlaxman <harithlaxman@gmail.com>
2024-03-04 15:52:21 +0530

    Added colorscheme - everforest

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,14 +1,27 @@
-return{
-  'rose-pine/neovim',
-  name = 'rose-pine',
+return {
+  --'rose-pine/neovim',
+  --name = 'rose-pine',
+  --opts = {
+  --  variant = "moon",
+  --  styles = {
+  --    transparency = true,
+  --  },
+  --},
+  --config = function(_, opts)
+  --  require("rose-pine").setup(opts)
+  --  vim.cmd("colorscheme rose-pine")
+  --end,
+  "neanias/everforest-nvim",
+  version = false,
+  lazy = false,
+  priority = 1000,
   opts = {
-    variant = "moon",
-    styles = {
-      transparency = true,
-    },
+    background = "soft",
+    transparent_background_level = 2,
+    italics = true,
   },
   config = function(_, opts)
-    require("rose-pine").setup(opts)
-    vim.cmd("colorscheme rose-pine")
+    require("everforest").setup(opts)
+    vim.cmd("colorscheme everforest")
   end,
 }
