bd9a98228f85a1afa5f7417052a662eeccb57539
harithlaxman <harithlaxman@gmail.com>
2024-04-01 10:29:16 +0530

    added remaps for splitting screen

--- lua/murph/remap.lua
+++ lua/murph/remap.lua
@@ -1,1 +1,4 @@
+vim.keymap.set("n", "<leader>vs", "<cmd>vsplit<CR>")
+vim.keymap.set("n", "<leader>hs", "<cmd>split<CR>")
+
 vim.keymap.set("n", "<C-b>", "<cmd>!g++ -std=c++17 -O2 -Wall % -o ex && ./ex<CR>")
