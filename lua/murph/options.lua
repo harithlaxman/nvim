0bb63463c66cef3fdb7d2e0f504ad9ed09505cdb
hgk <hgk@cisco.com>
2024-10-12 12:00:20 +0530

    Colorscheme + Blankline for indentation

--- lua/murph/options.lua
+++ lua/murph/options.lua
@@ -3,8 +3,8 @@ vim.opt.nu = true
 vim.opt.rnu = true
 
 vim.opt.expandtab = true
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 2
 
 vim.opt.smartindent = true
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,27 +1,50 @@
 return {
-  --'rose-pine/neovim',
-  --name = 'rose-pine',
+  'rose-pine/neovim',
+  name = 'rose-pine',
+  config = function(_)
+    require("rose-pine").setup({})
+    vim.cmd.colorscheme("rose-pine")
+  end,
+  --"neanias/everforest-nvim",
+  --version = false,
+  --lazy = false,
+  --priority = 1000,
   --opts = {
-  --  variant = "moon",
-  --  styles = {
-  --    transparency = true,
-  --  },
+  --  background = "soft",
+  --  transparent_background_level = 2,
+  --  italics = true,
   --},
   --config = function(_, opts)
-  --  require("rose-pine").setup(opts)
-  --  vim.cmd("colorscheme rose-pine")
+  --  require("everforest").setup(opts)
+  --  vim.cmd("colorscheme everforest")
   --end,
-  "neanias/everforest-nvim",
-  version = false,
-  lazy = false,
-  priority = 1000,
-  opts = {
-    background = "soft",
-    transparent_background_level = 2,
-    italics = true,
-  },
-  config = function(_, opts)
-    require("everforest").setup(opts)
-    vim.cmd("colorscheme everforest")
-  end,
+  --"gbprod/nord.nvim",
+  --lazy = false,
+  --priority = 1000,
+  --opts = {
+  --transparent = true,
+  --terminal_colors = true,
+  --borders = true,
+  --diff = { mode = "bg" },
+  --errors = { mode = "bg" },
+  --search = { theme = "vim" },
+  --styles = {
+  --comments = { italic = true }
+  --},
+  --},
+  --config = function(_, opts)
+  --require("nord").setup({})
+  --vim.cmd.colorscheme("nord")
+  --end,
+  --"catppuccin/nvim",
+  --name = "catppuccin",
+  --priority = 1000,
+  --opts = {
+  --flavour = "auto",
+  --transparent_background = false,
+  --},
+  --config = function(_, opts)
+  --require("catppuccin").setup(opts)
+  --vim.cmd.colorscheme("catppuccin")
+  --end
 }
